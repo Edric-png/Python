3f21ac6006a8bbfdf92d02451f9d641c4c5db461
Edric-png <braslinedric@gmail.com>
2025-11-14 13:43:21 +0400

    Add files via upload

---
+++ Birthday.py
@@ -0,0 +1,11 @@
+birthday1=2013, 5, 14
+birthday2=2014, 7, 19
+birthday3=2011, 1, 16
+birthday4=2013, 2, 26
+birthday5=2012, 11, 5
+# if necessary here print option
+print("Birthday 1 \n",birthday1)
+print("Birthday 2\n",birthday2)
+print("Birthday 3 \n",birthday3)
+print("Birthday 4 \n",birthday4)
+print("Birthday 5 \n",birthday5)
