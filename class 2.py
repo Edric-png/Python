a473cc9015f6e65da5543afa93d5caa9947c2f12
Edric-png <braslinedric@gmail.com>
2025-11-14 13:12:17 +0400

    Add files via upload

---
+++ class 2.py
@@ -0,0 +1,11 @@
+# print("Hello World")
+# # My name is Edric
+# name = "Edric"
+# age =   11
+# print("my name is",name)
+# print(11)
+# name=input("Enter your name ")
+# age=input("Enter your age ")
+# print("my name is \n",name,"\n my age is \n",age)
+import keyword
+print(keyword.kwlist)
