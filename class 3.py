97142ce0f5e2b0784a2dba747f7ffacde62bb520
Edric-png <braslinedric@gmail.com>
2025-11-21 13:29:16 +0400

    Add files via upload

---
+++ class 3.py
@@ -0,0 +1,18 @@
+# a = 10
+# print(type(a))
+# b = 5.29
+# print(type(b))
+# name = "Edric"
+# print(type(name))
+# c = True
+# print(type(c))
+# name = str(input("Enter your name"))
+# age = int(input("Enter your age"))
+# weight = float(input("Enter your weight"))
+# print(type(name),type(age),type(weight))
+# age = str(age)
+# weight = int(weight)
+# print(type(age),type(weight))
+a=str(input("Enter your name"))
+r=a[::-4]
+print(r)
