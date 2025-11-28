243e62c63a92bc88e39583458cb08d8dd8adac6b
Edric-png <braslinedric@gmail.com>
2025-11-28 13:27:16 +0400

    Add files via upload

---
+++ class 4.py
@@ -0,0 +1,17 @@
+# a=20
+# b=40
+# c=30
+# avg=(a+b+c)/3
+# print("avg =",avg)
+# a=720
+# note1=a//100
+# note2=(a%100)//50
+# note3=((a%100)%50)//10
+# print(note1,note2,note3)
+Eng=90
+Math=80
+IndSST=60
+Science=45
+UaeSST=68
+percentage=((Eng+Math+IndSST+Science+UaeSST)/500)*100
+print("% =",percentage)
