d9c3957dc7979443d19c05b16be1d5812d94c609
Edric-png <braslinedric@gmail.com>
2025-12-05 13:27:12 +0400

    Add files via upload

---
+++ class 5.py
@@ -0,0 +1,33 @@
+# a = 7
+# if a>0:
+#     print("Positive")
+# else:
+#     print("Negative")
+# Cost = float(input("Enter the cost"))
+# Amount = float(input("Enter the amount"))
+# if Amount>Cost:
+#     print("Profit",Amount-Cost)
+# else:
+#     print("No profit")
+# a = int(input("Enter any number"))
+# if a>15:
+#     print("a is greater than 15")
+#     print("I am in if")
+# else:
+#     print("a is not greater than 15")
+#     print("I am in else")
+# print("I am neither in if or else")
+# a = int(input("Enter the Number"))
+# if a%2==0:
+#     print("It is even")
+# else:
+#     print("It is odd")
+a = float(input("Enter your marks"))
+if a>=90:
+    print("A grade")
+elif a>=80:
+    print("B grade")
+elif a>=70:
+    print("C grade")
+else:
+    print("D grade")
