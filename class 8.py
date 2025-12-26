e43a7082c94653adfd3ae09274adda7670aa7718
Edric-png <braslinedric@gmail.com>
2025-12-26 13:31:20 +0400

    Add files via upload

---
+++ class 8.py
@@ -0,0 +1,19 @@
+# a = ord("T")
+# print(a)
+# a=53
+# b=90
+# c=28
+# d=48
+# add=((a+b)*c)/d
+# print(add)
+# a=57
+# b=84
+# if a%b==0:
+# print("It is divisible")
+# else:
+# print("It is not divisible")
+avg=38
+sum=avg*40
+sum=sum+20
+avg=sum/40
+print(avg)
