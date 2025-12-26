6e5e57e829df219dd1a4b76fa0fce9e89904cab8
Edric-png <braslinedric@gmail.com>
2025-12-26 15:14:45 +0400

    Add files via upload

---
+++ number swap.py
@@ -0,0 +1,11 @@
+# for example Tiya need to find the avg of 4 numbers but she mistakenly interchanged the value of 3 numbers.
+# she has read 33 as 23, 21 as 11, and 24 as 14.
+a=23
+b=14
+c=39
+d=11
+avg=(23+14+39+11)/4
+sum=avg*4
+sum=sum+10+10+10
+avg=sum/4
+print(avg)
