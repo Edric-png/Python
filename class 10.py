1751a0a5f5571089ff59b88fba1387aafd09f11a
Edric-png <braslinedric@gmail.com>
2026-01-09 13:28:31 +0400

    Add files via upload

---
+++ class 10.py
@@ -0,0 +1,13 @@
+# print("hello world \n " *10)
+# a = int(input("Enter any number"))
+# sum = 0
+# for i in range(1,a+1):
+#     sum = i+sum
+#     print("sum =",sum)
+# for i in range(10,0,-1):
+#     print(i)
+a = "Edric"
+b = ""
+for i in a:
+    b = i+b
+    print(b)
