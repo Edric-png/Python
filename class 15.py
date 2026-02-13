77f197d81b4e72bdcc60a2133c7ebcc89fee0f2d
Edric-png <braslinedric@gmail.com>
2026-02-13 13:11:29 +0400

    Add files via upload

---
+++ class 15.py
@@ -0,0 +1,32 @@
+# def a(name):
+#     print("Hello")
+#     print("My name is",name)
+#     return name
+# a("Edric")
+# def add(a,b):
+#     return a + b
+# def subtract(a,b):
+#     return a-b
+# def multiply(a,b):
+#     return a*b
+# def divide(a,b):
+#     return a/b
+# c = int(input("Enter the first number:"))
+# d = int(input("Enter the second number:"))
+# e = input("Press a for addition, b for subtraction, c for multiplication, d for division:")
+# if e=="a":
+#     print(add(c,d))
+# elif e=="b":
+#     print(subtract(c,d))
+# elif e=="c":
+#     print(multiply(c,d))
+# elif e=="d":
+#     print(divide(c,d))
+# else:
+#     print("This is invalid input")
+def a():
+    print("The weather is",s)
+    print("The weather is ",b)
+s = "autumn"
+b = s
+a()
