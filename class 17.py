65e31ed0412f078c9da9b7f7d43ff0fb2a646d15
Edric-png <braslinedric@gmail.com>
2026-02-27 13:15:02 +0400

    Add files via upload

---
+++ class 17.py
@@ -0,0 +1,23 @@
+# str = "Pankaj"
+# for i in str:
+#     if i=="a":
+#         print("a found")
+#         break
+#     else:
+#         pass
+# for i in range(20):
+#     if i%20==0:
+#         print("twist")
+#     elif i%15==0:
+#         pass
+#     elif i%5==0:
+#         print("fizz")
+#     elif i%3==0:
+#         print("buzz")
+#     else:
+#         print(i)
+for i in range(10,0,-1):
+    if i==5:
+        continue
+    else:
+        print(i)
