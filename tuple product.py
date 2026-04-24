a3f8ccecc99223a0caa0439eeadebb25c545e6ed
Edric-png <braslinedric@gmail.com>
2026-04-24 13:26:54 +0400

    Add files via upload

---
+++ tuple product.py
@@ -0,0 +1,4 @@
+tuple1 = (2,4,8,8,3,2,9)
+print("the product of all the numbers in tuple1 are:",2*4*8*8*3*2*9)
+tuple2 = (4,3,2,2,-1,18)
+print("the product of all the numbers in tuple2 are:",4*3*2*2*-1*18)
