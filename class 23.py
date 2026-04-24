6903a292bb06eaaa832391f8df082ac248735899
Edric-png <braslinedric@gmail.com>
2026-04-24 13:12:36 +0400

    Update class 23.py

--- class 23.py
+++ class 23.py
@@ -13,11 +13,18 @@
 #         result[student_id] = details
 # for k, v in result.items():
 #     print(k, ":", v)
-test_dict = {'Coding' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coded' : 1}
-print("The original dictionary : " + str(test_dict))
-K = 2
-res = 0
-for key in test_dict:
-    if test_dict[key] == K:
-        res = res + 1
-print("Frequency of K is : " + str(res))
+# test_dict = {'Coding' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coded' : 1}
+# print("The original dictionary : " + str(test_dict))
+# K = 2
+# res = 0
+# for key in test_dict:
+#     if test_dict[key] == K:
+#         res = res + 1
+# print("Frequency of K is : " + str(res))
+country_code = {'India' : '0091',
+'Australia' : '0025',
+'Nepal' : '00977'}
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
